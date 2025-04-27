32c2cb991f273a29216c1ea335021f861def0af9
luminous44 <aklemasultanakhi@gmail.com>
2025-04-27 16:15:16 +0600

    added employee details

---
+++ Employee.java
@@ -0,0 +1,31 @@
+
+package remuflow;
+
+abstract class Employee {
+    private String name;
+    private String category;
+    private int id;
+    
+    Employee(String name, int id, String cat){
+        this.name = name;
+        this.id = id;
+        this.category = cat;
+    }
+    public String name(){
+        return name;
+    }
+    public String category(){
+        return category;
+    }
+    public int getId(){
+       return id; 
+    } 
+    abstract public double calculateSalary();
+    
+    @Override
+    public String toString(){
+        System.out.println("");
+        return "ID : "+id+"\nName : "+name+"\nCategory : "+category+"\nSalary : "+calculateSalary()+" ";  
+    }
+
+}
