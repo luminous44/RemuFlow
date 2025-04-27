05af21a73b8096f8f87087b432573c30211746e7
luminous44 <aklemasultanakhi@gmail.com>
2025-04-27 19:18:02 +0600

    added full time emplyoee

---
+++ FulllTimeEmployee.java
@@ -0,0 +1,18 @@
+
+package remuflow;
+
+
+public class FulllTimeEmployee extends Employee{
+    
+    double monthlySalary;
+    
+     FulllTimeEmployee(String name, int id,String cat, double salary){
+         super(name,id,cat);
+         this.monthlySalary = salary;
+     }
+    @Override
+    public double calculateSalary() {
+        return monthlySalary;
+    }
+       
+}
